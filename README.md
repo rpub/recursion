ec0937077d17044a5b7c26b761e4b65ff6528fe2
Carlos Roberto Arias Arevalo <32964401+arias-spu@users.noreply.github.com>
2020-05-16 21:24:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Recursion Exercises
+Four Recursive Function Exercises
+
+Do the following exercises:
+1. Write a recursive function that calculates the nth number in the Fibonacci Sequence
+1. Write a recursive function that takes in a string and outputs the string in reverse. (Do
+ not use stack)
+1. Write a recursive function that finds the position of the minimum element in an array
+1. Write a recursive function that sorts an array using selection sort algorithm
+
+**Note**
+It is VERY important that you understand how to get to the solution of these problems, you
+ may work with your classmates, we are not going to check if you copied the code, so it will
+  be on you that you understand the concepts and how to solve this.
+  
+## Given
+You are given a main function and the declaration and stubs of the functions. You may not
+ change the main function except maybe the parameters of the functions if you decide to
+  change them. *It is possible to write this program without changing the parameters of the
+   given functions*. You may add functions to your program.
+
+## Expected Output
+
+Your output should be like this:
+```
+Testing Fibonacci
+	Passed 1 tests
+	Passed 2 tests
+	Passed 3 tests
+	Passed 4 tests
+	Passed 5 tests
+	Passed 6 tests
+	Passed 7 tests
+	Passed 8 tests
+	Passed 9 tests
+	Passed 10 tests
+Testing Minimum Position Finding
+	Passed 11 tests
+	Passed 12 tests
+	Passed 13 tests
+Testing Sorting
+	Passed 14 tests
+	Passed 15 tests
+	Passed 16 tests
+Testing Reverse Strings
+	Passed 17 tests
+	Passed 18 tests
+	Passed 19 tests
+```
---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::stringstream ;
+using std::string;
+using std::ostream;
+
+unsigned long long int Fibonacci(unsigned int n);
+void PrintReverseString(const string& str, ostream& output = cout);
+// You may change the parameters of these functions
+size_t MinimumPosition(const int array[], size_t size);
+void SelectionSort(int array[], size_t size);
+
+
+int main() {
+	const size_t ELEMENTS = 7;
+	const size_t TESTS = 3;
+	int data[][ELEMENTS] = {
+			{7, 5, 4, 1, 3, 0, 9},
+			{1, 5, 9, 11, 13, 20, 29},
+			{8, 5, 4, 1, 3, 4, 0}};
+
+	int minimumSolutions[] = {5, 0, 6};
+	int sortedArrays[][7] = {
+			{0, 1, 3, 4, 5, 7, 9},
+			{1, 5, 9, 11, 13, 20, 29},
+			{0, 1, 3, 4, 4, 5, 8}};
+
+	unsigned long long int fibonacciSequence[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+
+	string strings[] = {"hello", "aardvark", "reviver"};
+	string reverses[] = {"olleh", "kravdraa", "reviver"};
+	size_t passed = 0;
+	int minimumResponses[TESTS];
+	for (int i = 0; i < TESTS; ++i) {
+		minimumResponses[i] = -1;
+	}
+
+	cout << "Testing Fibonacci" << endl;
+	for (int i = 0; i < 10; ++i) {
+		unsigned long long int fibonacci = Fibonacci(i);
+		if (fibonacci == fibonacciSequence[i])
+			cout << "\tPassed " << ++passed << " tests" << endl;
+	}
+
+
+	cout << "Testing Minimum Position Finding" << endl;
+	for (int i = 0; i < TESTS; ++i) {
+		minimumResponses[i] = MinimumPosition(data[i], ELEMENTS);
+		if (minimumResponses[i] == minimumSolutions[i]){
+			cout << "\tPassed " << ++passed << " tests" << endl;
+		}
+	}
+	cout << "Testing Sorting" << endl;
+	bool equal;
+	for (int i = 0; i < TESTS; ++i) {
+		SelectionSort(data[i], ELEMENTS);
+		equal = true;
+		for (int j = 0; j < ELEMENTS; ++j) {
+			if (data[i][j] != sortedArrays[i][j]){
+				equal = false;
+				cerr << "Test " << i << " position " << j << " values: ";
+				cerr << data[i][j] << " " << sortedArrays[i][j] << endl;
+				break;
+			}
+		}
+		if (equal){
+			cout << "\tPassed " << ++passed << " tests" << endl;
+		}
+	}
+	cout << "Testing Reverse Strings" << endl;
+	stringstream out;
+	for (int i = 0; i < TESTS; ++i) {
+		out.clear();
+		out.str("") ;
+		PrintReverseString(strings[i], out);
+		if (out.str() == reverses[i])
+			cout << "\tPassed " << ++passed << " tests" << endl;
+	}
+
+	return 0;
+}
+
+unsigned long long int Fibonacci(unsigned int n){
+	return 0;
+}
+void PrintReverseString(const string& str, ostream& output){
+}
+// You may change the parameters of these functions
+size_t MinimumPosition(const int array[], size_t size){
+	return 0;
+}
+void SelectionSort(int array[], size_t size){
+
+}
